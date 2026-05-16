607d1409d7ff4367b6946c38941c58f69a82f108
Ayther1 <ayther1234@gmail.com>
2026-05-16 16:57:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -14,13 +14,6 @@ KICHE_NUMBER = "7112676056"
 
 # روابط الصور
 IMAGES = {
-    "welcome":  "https://files.catbox.moe/0vz26u.jpeg",
-    "news":     "https://files.catbox.moe/oh3q4f.jpeg",
-    "daily":    "https://files.catbox.moe/twu7tg.jpeg",
-    "weekly":   "https://files.catbox.moe/270cr1.jpeg",
-    "monthly":  "https://files.catbox.moe/j0l4bd.jpeg",
-    "open":     "https://files.catbox.moe/blancp.jpeg",
-    "search":   "https://files.catbox.moe/vsqp2z.jpeg",
     "payment":  "https://files.catbox.moe/uuu6qm.jpeg",
 }
 
