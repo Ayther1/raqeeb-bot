674e485be8c683ae8ce33f5cc5d3fca0bd6f6356
Ayther1 <ayther1234@gmail.com>
2026-05-17 07:30:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,7 @@ ADMIN_ID     = 8601067589
 CHANNEL_ID   = "@RaqeebIQ"
 SUPPORT_BOT  = "@RaqeebAskBot"
 KICHE_NUMBER = "7112676056"
-
-IMAGES = {
-    "welcome":  "https://files.catbox.moe/0vz26u.jpeg",
-    "news":     "https://files.catbox.moe/oh3q4f.jpeg",
-    "daily":    "https://files.catbox.moe/twu7tg.jpeg",
-    "weekly":   "https://files.catbox.moe/270cr1.jpeg",
-    "monthly":  "https://files.catbox.moe/j0l4bd.jpeg",
-    "open":     "https://files.catbox.moe/blancp.jpeg",
-    "search":   "https://files.catbox.moe/vsqp2z.jpeg",
-    "payment":  "https://files.catbox.moe/uuu6qm.jpeg",
-}
+QR_IMAGE     = "https://files.catbox.moe/uuu6qm.jpeg"
 
 PRICES = {
     "monthly": 5000,
@@ -26,9 +16,10 @@ PRICES = {
 
 TRIAL_DAYS = 7
 
-# حدود مراقبة الشركات حسب نوع الاشتراك
 WATCH_LIMITS = {
     "trial":   1,
     "monthly": 5,
     "yearly":  10,
 }
+
+ISX_URL = "http://www.isx-iq.net/isxportal/portal"
